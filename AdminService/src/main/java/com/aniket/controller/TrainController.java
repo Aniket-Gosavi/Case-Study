4e91226ca481464596ed56e253c3933c1a728eb4
Aniket Gosavi <106654078+Aniket-Gosavi@users.noreply.github.com>
2023-06-23 13:12:46 +0530

    test cases added

--- AdminService/src/main/java/com/aniket/controller/TrainController.java
+++ AdminService/src/main/java/com/aniket/controller/TrainController.java
@@ -8,18 +8,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aniket.exception.ResourceNotFoundException;
 import com.aniket.model.TrainDetails;
-import com.aniket.service.TrainService;
+import com.aniket.service.TrainServiceImpl;
 
 @RestController
 public class TrainController {
 	
 	@Autowired
-	TrainService ts; 
+	TrainServiceImpl ts;
 	
 	@PostMapping("/add")
 	public ResponseEntity<?> addTrain(@RequestBody TrainDetails td){
--- AdminService/src/main/java/com/aniket/service/TrainServiceImpl.java
+++ AdminService/src/main/java/com/aniket/service/TrainServiceImpl.java
@@ -1,13 +1,58 @@
 package com.aniket.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.aniket.exception.ResourceNotFoundException;
 import com.aniket.model.TrainDetails;
+import com.aniket.repository.TrainRepo;
 
-public interface TrainServiceImpl {
-	TrainDetails addTrain(TrainDetails td);
-	List<TrainDetails> showTrains();
-	TrainDetails updateTrain(int trainNo, String destination) throws ResourceNotFoundException;
-	String deleteTrain(int id) throws ResourceNotFoundException;
+@Service
+public class TrainServiceImpl implements TrainServiceI {
+
+	private static final Logger log = LoggerFactory.getLogger(TrainServiceImpl.class);
+
+	@Autowired
+	private TrainRepo trepo;
+
+	@Override
+	public TrainDetails addTrain(TrainDetails td) {
+		TrainDetails save = trepo.save(td);
+		return save;
+	}
+
+	@Override
+	public List<TrainDetails> showTrains() {
+		return trepo.findAll();
+	}
+
+	@Override
+	public TrainDetails updateTrain(int id, String destination) throws ResourceNotFoundException {
+		Optional<TrainDetails> td = trepo.findById(id);
+		if(td.isEmpty()) {
+			log.warn("Train with the specified Id is not avvailable" +id);
+			throw new ResourceNotFoundException("Train with the following Id Does not Exist " +id);
+		}
+		TrainDetails tds = td.get();
+		tds.setDestination(destination);
+		TrainDetails save = trepo.save(tds);
+		return save;
+	}
+
+	@Override
+	public String deleteTrain(int id) throws ResourceNotFoundException {
+		Optional<TrainDetails> td = trepo.findById(id);
+		if(td.isEmpty()) {
+			throw new ResourceNotFoundException("Train with the following Id Does not Exist " +id);
+		}
+		TrainDetails tds = td.get();
+		trepo.delete(tds);
+		return "Deleted Successfully";
+	}
+
 }
